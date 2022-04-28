01ee75a70351e258e3d9d39fed13d6ae3ed4dc5b
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-04-28 17:48:52 +0300

    Add clang language server and configuration

---
+++ lua/lsp/settings/clangd.lua
@@ -0,0 +1,30 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = { "documentation", "detail", "additionalTextEdits" },
+}
+
+local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
+local clangd_capabilities = cmp_capabilities
+clangd_capabilities.textDocument.semanticHighlighting = true
+
+clangd_capabilities.offsetEncoding = "utf-8"
+return {
+    capabilities = clangd_capabilities,
+    cmd = {
+    	"clangd",
+    	"--background-index",
+    	"--pch-storage=memory",
+    	"--clang-tidy",
+    	"--suggest-missing-includes",
+    	"--cross-file-rename",
+    	"--completion-style=detailed",
+    },
+    init_options = {
+    	clangdFileStatus = true,
+    	usePlaceholders = true,
+    	completeUnimported = true,
+    	semanticHighlighting = true,
+    }
+}
