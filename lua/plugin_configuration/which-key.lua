9256187b8625f73954675385049bd301594f5933
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-05-07 16:02:24 +0300

    Enable spelling plugin for which-key

--- lua/plugin_configuration/which-key.lua
+++ lua/plugin_configuration/which-key.lua
@@ -6,6 +6,26 @@ if not status_ok then
 	return
 end
 
+wk.setup({
+	plugins = {
+		marks = true,
+		registers = true,
+		spelling = {
+			enabled = true,
+			suggestions = 20,
+		},
+		presets = {
+			operators = true,
+			motions = true,
+			text_objects = true,
+			windows = true,
+			nav = true,
+			z = true,
+			g = true,
+		}
+	}
+})
+
 local opts = {
 	mode = "n",
 	prefix = "",
