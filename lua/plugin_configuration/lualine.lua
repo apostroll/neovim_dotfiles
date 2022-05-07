1dc6194cacb9c7a3d3ff065f865b62648197cc1f
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-05-07 16:25:39 +0300

    Remove unused buggy code

--- lua/plugin_configuration/lualine.lua
+++ lua/plugin_configuration/lualine.lua
@@ -33,9 +33,6 @@ end
 local function process_sections(sections)
   for name, section in pairs(sections) do
     local left = name:sub(9, 10) < 'x'
-    for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
-      table.insert(section, pos * 2, { empty, color = { fg = '#f3f3f3', bg = '#f3f3f3' } })
-    end
     for id, comp in ipairs(section) do
       if type(comp) ~= 'table' then
         comp = { comp }
