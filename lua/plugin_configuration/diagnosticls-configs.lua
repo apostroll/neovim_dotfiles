ea527f807d6c27b9d6f1e89be72274bbf8586514
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-05-08 21:38:49 +0300

    fix formatting inconsistency

--- lua/plugin_configuration/diagnosticls-configs.lua
+++ lua/plugin_configuration/diagnosticls-configs.lua
@@ -3,7 +3,7 @@ if not status_ok then
 	return
 end
 
-local shellcheck = require 'diagnosticls-configs.linters.shellcheck'
+local shellcheck = require('diagnosticls-configs.linters.shellcheck')
 local rubocop = require('diagnosticls-configs.linters.rubocop')
 
 dlsconfig.setup {
