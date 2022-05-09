a19e584cc9a1ca02356936387c5ef1f65fbf16ea
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-05-09 20:33:38 +0300

    Enable null-ls buffer formatting on save

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -15,6 +15,8 @@ if not status_keymaps_ok then
 	return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	sources = {
 		formatting.prettier.with({ extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' } }),
@@ -36,5 +38,16 @@ null_ls.setup({
 		if client.name == 'null-ls' then
 			keymaps.lsp_keymaps(bufnr)
 		end
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
 	end
 })
