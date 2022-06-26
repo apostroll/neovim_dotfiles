8e2d68ca309a60302e114df54e0505cac1095ee1
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-06-27 00:18:25 +0300

    Revert formatting on buffer save
    This future is more often more annoying than it is helpful

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -15,8 +15,6 @@ if not status_keymaps_ok then
 	return
 end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 null_ls.setup({
 	sources = {
 		-- formatting
@@ -49,16 +47,5 @@ null_ls.setup({
 		if client.name == 'null-ls' then
 			keymaps.lsp_keymaps(bufnr)
 		end
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
 	end
 })
