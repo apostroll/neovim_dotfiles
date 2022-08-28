c5d48b0356e831f906eb474d321b1c79c60fcdef
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-08-28 17:25:11 +0300

    toggleterm: Press <esc> in terminal to enter normal mode

--- lua/plugin_configuration/toggleterm.lua
+++ lua/plugin_configuration/toggleterm.lua
@@ -3,9 +3,16 @@ if not status_ok then
 	return
 end
 
-toggleterm.setup{
-  shade_terminals = true,
-  start_in_insert = true,
-  persist_size = true,
-  close_on_exit = true,
+toggleterm.setup {
+	shade_terminals = true,
+	start_in_insert = true,
+	persist_size = true,
+	close_on_exit = true,
 }
+
+function _G.set_terminal_keymaps()
+	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { buffer = 0 })
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
