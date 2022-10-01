4b4dfe5395e87252d8a28c0333b5a72e3efc0232
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-10-01 15:42:24 +0300

    Lualine: Re-indent configuration

--- lua/plugin_configuration/lualine.lua
+++ lua/plugin_configuration/lualine.lua
@@ -45,27 +45,27 @@ local function process_sections(sections)
 end
 
 local function mixed_indent()
-  local space_pat = [[\v^ +]]
-  local tab_pat = [[\v^\t+]]
-  local space_indent = vim.fn.search(space_pat, 'nwc')
-  local tab_indent = vim.fn.search(tab_pat, 'nwc')
-  local mixed = (space_indent > 0 and tab_indent > 0)
-  local mixed_same_line
-  if not mixed then
-    mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
-    mixed = mixed_same_line > 0
-  end
-  if not mixed then return '' end
-  if mixed_same_line ~= nil and mixed_same_line > 0 then
-     return 'Mixed Indent:'..mixed_same_line
-  end
-  local space_indent_cnt = vim.fn.searchcount({pattern=space_pat, max_count=1e3}).total
-  local tab_indent_cnt =  vim.fn.searchcount({pattern=tab_pat, max_count=1e3}).total
-  if space_indent_cnt > tab_indent_cnt then
-    return 'Mixed Indent:'..tab_indent
-  else
-    return 'Mixed Indent:'..space_indent
-  end
+	local space_pat = [[\v^ +]]
+	local tab_pat = [[\v^\t+]]
+	local space_indent = vim.fn.search(space_pat, 'nwc')
+	local tab_indent = vim.fn.search(tab_pat, 'nwc')
+	local mixed = (space_indent > 0 and tab_indent > 0)
+	local mixed_same_line
+	if not mixed then
+		mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+		mixed = mixed_same_line > 0
+	end
+	if not mixed then return '' end
+	if mixed_same_line ~= nil and mixed_same_line > 0 then
+		return 'Mixed Indent:' .. mixed_same_line
+	end
+	local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
+	local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
+	if space_indent_cnt > tab_indent_cnt then
+		return 'Mixed Indent:' .. tab_indent
+	else
+		return 'Mixed Indent:' .. space_indent
+	end
 end
 
 lualine.setup({
@@ -80,9 +80,9 @@ lualine.setup({
 		lualine_a = { 'mode' },
 		lualine_b = { branch, 'diff' },
 		lualine_c = {
-			{'filename', path = 2, files_status = true}
+			{ 'filename', path = 2, files_status = true }
 		},
-		lualine_x = {mixed_indent},
+		lualine_x = { mixed_indent },
 		lualine_y = { 'filetype', 'encoding', 'fileformat' },
 		lualine_z = { diagnostics, spaces },
 	}),
