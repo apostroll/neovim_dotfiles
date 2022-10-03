3fb80307001ebd3aa545e065f25bf487a6fed728
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2022-10-03 23:01:08 +0300

    Replace vim.lsp.buf.formatting_sync with vim.lsp.buf.format

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -1,14 +1,14 @@
 M = {}
 
 M.lsp_keymaps = function(bufnr)
-	local status_ok, wk = pcall(require, 'which-key')
+	local status_ok, wk = pcall(require, "which-key")
 	if not status_ok then
 		return
 	end
 
 	local opts = {
-		mode = 'n',
-		prefix = '',
+		mode = "n",
+		prefix = "",
 		buffer = bufnr,
 		silent = true,
 		noremap = true,
@@ -16,22 +16,22 @@ M.lsp_keymaps = function(bufnr)
 	}
 
 	wk.register({
-		['g'] = {
-			name = 'Code Navigation',
-			D = { '<cmd>lua vim.lsp.buf.declaration()<CR>', 'GoTo Declaration' },
-			d = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'GoTo Definition' },
-			i = { '<cmd>lua vim.lsp.buf.implementation()<CR>', 'GoTo Implementation' },
-			r = { '<cmd>lua vim.lsp.buf.references()<CR>', 'GoTo References' },
-			n = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'GoTo Next Diagnostic' },
-			p = { '<cmd>lua vim.diagnostic.goto_prev()<CR>', 'GoTo Previous Diagnostic' },
-			l = { '<cmd>lua vim.diagnostic.setloclist()<CR>', 'GoTo Diagnostics List' },
-			a = { '<cmd>lua vim.lsp.buf.code_action()<CR>', 'Code Actions' },
+		["g"] = {
+			name = "Code Navigation",
+			D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "GoTo Declaration" },
+			d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "GoTo Definition" },
+			i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "GoTo Implementation" },
+			r = { "<cmd>lua vim.lsp.buf.references()<CR>", "GoTo References" },
+			n = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "GoTo Next Diagnostic" },
+			p = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "GoTo Previous Diagnostic" },
+			l = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "GoTo Diagnostics List" },
+			a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Actions" },
 		},
-		['K'] = { '<cmd>lua vim.lsp.buf.hover()<CR>', 'Show Documentation' },
-		['<C-k>'] = { '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature Help' },
-		['<localleader>'] = {
-			['d'] = { '<cmd>lua vim.diagnostic.open_float()<CR>', 'Floating Diagnostic' },
-			['f'] = { '<cmd>lua vim.lsp.buf.formatting_sync()<cr>', 'Format buffer' },
+		["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Show Documentation" },
+		["<C-k>"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+		["<localleader>"] = {
+			["d"] = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Floating Diagnostic" },
+			["f"] = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format buffer" },
 		},
 	}, opts)
 end
