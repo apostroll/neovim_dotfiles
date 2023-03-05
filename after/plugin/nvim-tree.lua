7d3c60c8d5e080884596e93871bdef7e7898b095
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2023-03-05 18:16:49 +0200

    nvim-tree: Replace `s` keymap

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -3,7 +3,19 @@ if not status_ok then
 	return
 end
 
+local api = require("nvim-tree.api")
+
+local M = {}
+
+M.on_attach = function(bufnr)
+	local opts = function(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	vim.keymap.set("n", "s", api.node.open.vertical, opts("Open: Vertical Split"))
+end
+
 nvim_tree.setup({
+	on_attach = M.on_attach,
 	filters = {
 		dotfiles = true,
 	},
