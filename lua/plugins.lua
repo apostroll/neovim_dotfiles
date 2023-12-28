00df292adaba68e75a077b842158642152550c26
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2023-12-28 23:28:03 +0200

    Replace archived null-ls with none-ls

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -111,7 +111,8 @@ lazy.setup({
 		-- Diagnostics
 		"jay-babu/mason-null-ls.nvim",
 		dependencies = {
-			"jose-elias-alvarez/null-ls.nvim" ,
+			"nvimtools/none-ls.nvim",
+			--[[ "jose-elias-alvarez/null-ls.nvim" , ]]
 		},
 	},
 	{
